a07e674f7f03d73a854bfc0d1fb982d6f2bcfd81
swapnil-kapile-dipakrao-db <139096339+swapnil-kapile-dipakrao-db@users.noreply.github.com>
2024-03-19 15:28:28 +0530

    Update quickstart.sh
    cloud build

--- quickstart-build/quickstart.sh
+++ quickstart-build/quickstart.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 # [START cloudbuild_quickstart_build_sh]
-# echo "Hello, world! The time is $(date)."
+ echo "Hello, world! The time is $(date)."
 # [END cloudbuild_quickstart_build_sh]
